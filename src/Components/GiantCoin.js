804134c1c81ff6f8ea47be1b1ada2b2359156e99
Raymond <you@example.com>
2021-07-13 18:04:10 -0700

    Updated the Hero Section but still needs some work

--- src/Components/GiantCoin.js
+++ src/Components/GiantCoin.js
@@ -7,9 +7,6 @@ function GiantCoint(){
         <div className="GiantCoin">
             <div className="GiantCoin_Circle_Container">
                 <div className="GiantCoin_Circle_Outer">
-                    <div className="GiantCoun_Circle_Inner">
-                        
-                    </div>
                 </div>
             </div>
         </div>
